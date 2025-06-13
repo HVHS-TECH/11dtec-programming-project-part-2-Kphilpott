8c91ca8d52aaf3ed31e79e063e6c5e7e77d2166b
Kphilpott <23424kp@hvhs.school.nz>
2025-06-13 03:06:03 +0000

    test3

--- page.js
+++ page.js
@@ -123,11 +123,13 @@ function drivingAge() {
 }
 //start program. what, were you expecting more?
 function start() {
-    document.getElementById("startpage").style.display = "none";
-    document.getElementById("startimg").style.display = "none";
-    document.getElementById("mainbutton").style.display = "none";
     
-   alert("Welcome to the starship rental interface. Minimum rental lengh is one (1) year.\nPlease note that you must be at least 25 years old to rent a ship.\n\nFour (4) ship types avaliable in normal section.\nFour (4) ships types avaliable in expensive catalog.\n\n\n© Luna Starships 2079 || Last Updated: 23.11.2079");
+    document.getElementById("startimg").style.display = "none";
+    
+    document.getElementById("startpage").innerHTML = "<h1>Welcome to Luna Starships: Rental Division</h1><h3>Your Leader in Space Travel & Rentals since 2079</h3>";
+    document.getElementById("mainbutton").innerHTML = <button class="w3-button w3-????????? w3-xxlarge" type="button"
+      onclick="start() ;">START</button>
+    alert("Welcome to the starship rental interface. Minimum rental lengh is one (1) year.\nPlease note that you must be at least 25 years old to rent a ship.\n\nFour (4) ship types avaliable in normal section.\nFour (4) ships types avaliable in expensive catalog.\n\n\n© Luna Starships 2079 || Last Updated: 23.11.2079");
    askUserName();
    askYear();
    askUserAge();
