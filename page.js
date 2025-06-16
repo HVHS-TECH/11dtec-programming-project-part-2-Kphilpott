352c98753118fa26e746a9c05d3067cf767f2f6a
Kphilpott <23424kp@hvhs.school.nz>
2025-06-16 21:23:52 +0000

    Jun 17, 2025, 9:23 AM

--- page.js
+++ page.js
@@ -282,33 +282,6 @@ function outputEXP() {
 
 
 
-function getFormInput() {
-    var message = "Welcome to the page ";
-    message = message + input_name.value;
-    welcomeMessage.innerHTML = message;
-}
-
-
-
-function getSelectInput() {
-    var message = "Welcome to the page ";
-    message = message + input_name.value;
-    message = message + ". You chose " + cars.value
-    welcomeMessage.innerHTML = message;
-
-    cars.innerHTML += '<option value="BYD">BYD</option>'
-}
-
-listOfFruit = ['strawberries', 'kiwifruit', 'fejoas', 'kiwana'];
-function buildDropdown() {
-    for (i = 0; i < listOfFruit.length; i++) {
-        var newOption = '<option value="' + listOfFruit[i] + '">' + listOfFruit[i] + '</option>';
-        fruitSelect.innerHTML += newOption;
-    }
-}
-
-
-
 
 
 
